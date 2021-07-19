62bd766ed72620f533caec6e48137b054c4e7259
Florent Clairambault <florent@clairambault.fr>
2021-07-19 21:55:26 +0200

    Coverage improvement (#2)
    * Tiny CI changes
    
    * Updating golangci-lint

--- gokit/gokit_test.go
+++ gokit/gokit_test.go
@@ -1,6 +1,7 @@
 package gokit
 
 import (
+	"os"
 	"testing"
 
 	"github.com/fclairamb/go-log/logtest"
@@ -8,4 +9,15 @@ import (
 
 func TestGoKit(t *testing.T) {
 	logtest.TestLogger(t, NewGKLoggerStdout())
+}
+
+type badLogger struct{}
+
+func (badLogger) Log(keyvals ...interface{}) error {
+	return os.ErrInvalid
+}
+
+func TestError(t *testing.T) {
+	logger := NewGKLogger(badLogger{})
+	logger.Info("test")
 }
--- logtest/logtest.go
+++ logtest/logtest.go
@@ -12,10 +12,11 @@ import (
 // TestLogger is shared test function
 func TestLogger(t *testing.T, logger log.Logger) {
 	logger = logger.With("test", t.Name())
-	logger.Debug("This is debug")
-	logger.Info("This is info")
-	logger.Warn("This is warning")
-	logger.Error("This is error")
-	logger.Info("Event", "key", "val")
+	logger.Debug("This is debug", "key", "val")
+	logger.Info("This is info", "key", "val")
+	logger.Warn("This is warning", "key", "val")
+	logger.Error("This is error", "key", "val")
+
+	// Buggy logging
 	logger.Info("Event", "key1", "val1", "key2")
 }
