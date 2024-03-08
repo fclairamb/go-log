08b1ce9f0600ef80179a944a7cd13c64daaa3733
fclairamb <florent.clairambault@gmail.com>
2024-03-08 23:09:41 +0100

    update

--- slog/slog.go
+++ slog/slog.go
@@ -5,39 +5,33 @@ import (
 	"context"
 	"fmt"
 
-	stdLog "log/slog"
+	stdSLog "log/slog"
 
 	"github.com/fclairamb/go-log"
 )
 
-func (logger *sLogger) checkError(err error) {
-	if err != nil {
-		fmt.Println("Logging faced this error: ", err)
-	}
-}
-
-func (logger *sLogger) log(level stdLog.Level, event string, keyvals ...interface{}) {
+func (logger *sLogger) log(level stdSLog.Level, event string, keyvals ...interface{}) {
 	logger.logger.Log(logger.ctx, level, event, keyvals...)
 }
 
 // Debug logs key-values at debug level
 func (logger *sLogger) Debug(event string, keyvals ...interface{}) {
-	logger.log(stdLog.LevelDebug, event, keyvals...)
+	logger.log(stdSLog.LevelDebug, event, keyvals...)
 }
 
 // Info logs key-values at info level
 func (logger *sLogger) Info(event string, keyvals ...interface{}) {
-	logger.log(stdLog.LevelInfo, event, keyvals...)
+	logger.log(stdSLog.LevelInfo, event, keyvals...)
 }
 
 // Warn logs key-values at warn level
 func (logger *sLogger) Warn(event string, keyvals ...interface{}) {
-	logger.log(stdLog.LevelWarn, event, keyvals...)
+	logger.log(stdSLog.LevelWarn, event, keyvals...)
 }
 
 // Error logs key-values at error level
 func (logger *sLogger) Error(event string, keyvals ...interface{}) {
-	logger.log(stdLog.LevelError, event, keyvals...)
+	logger.log(stdSLog.LevelError, event, keyvals...)
 }
 
 func (logger *sLogger) Panic(event string, keyvals ...interface{}) {
@@ -48,23 +42,23 @@ func (logger *sLogger) Panic(event string, keyvals ...interface{}) {
 
 // With adds key-values
 func (logger *sLogger) With(keyvals ...interface{}) log.Logger {
-	//return NewWrap(slog.With(logger.logger, keyvals...))
 	return NewWrap(logger.logger.With(keyvals...))
 }
 
 // NewWrap creates a logger based on go-kit logs
-func NewWrap(logger *stdLog.Logger) log.Logger {
+func NewWrap(logger *stdSLog.Logger) log.Logger {
 	return &sLogger{
 		logger: logger,
+		ctx:    context.Background(),
 	}
 }
 
 // New creates a logger based on go-kit logs but with some default parameters
 func New() log.Logger {
-	return NewWrap(stdLog.Default())
+	return NewWrap(stdSLog.Default())
 }
 
 type sLogger struct {
-	logger *stdLog.Logger
+	logger *stdSLog.Logger
 	ctx    context.Context
 }
